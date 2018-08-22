c550020aa6684f6522bbaa669eead8da2ac1719f
Joe Saraceno <honestcomrade@gmail.com>
2018-08-22 08:33:02 -0700

    testing + comments

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -2,10 +2,15 @@
 
 echo "Waiting for Postgres init..."
 
+# netcat will send empty traffic to the users-db host
+# while that is not able to connect, sleep and try again
+# then finish
 while ! nc -z users-db 5432; do
   sleep 0.1
 done
 
 echo "PostgreSQL started on port 5432"
 
+# now that we have made connection with the pg db
+# we can run the manage script
 python manage.py run -h 0.0.0.0
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,13 +1,17 @@
 import os
 
 
+# Set the base configuration that all the other configs willinherit from
 class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'sshhhh'
 
 
-
+# Pass the baseConfig object and extend it. in this case we add the SQLALCHEMY_DATABSE_URI key
+# and assign it to the value that the environment variable knows about
+# which itself comes from the docker-compose file
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
