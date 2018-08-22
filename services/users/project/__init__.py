1fbfed7b3f2499c32d8cd49460b87dfba285f126
Joe Saraceno <honestcomrade@gmail.com>
2018-08-21 22:18:57 -0700

    postgres connect adn build table

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,35 +1,36 @@
-import os # needed for the app_settings to get
-          # pulled in as from the container's
-          # env variable
+import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-# init Flask app
-app = Flask(__name__);
+
+# instantiate the app
+app = Flask(__name__)
 
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
-# init the db from app
+# instantiate the db
 db = SQLAlchemy(app)
 
-# user schema
+
+# model
 class User(db.Model):
-  __tablename__ = 'users'
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  username = db.Column(db.String(128), nullable=False)
-  email = db.Column(db.String(128), nullable=False)
-  active = db.Column(db.Boolean(), default=True, nullable=False)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
 
-  def __init__(self, username, email):
-    self.username = username
-    self.email = email
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
-# app routes
+
+# routes
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
-      'status':'success',
-      'message':'pong!'
+        'status': 'success',
+        'message': 'pong!'
     })
