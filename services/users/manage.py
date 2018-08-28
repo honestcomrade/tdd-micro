aa3807018ffdd9787bac34bc90352dd4278cb4a2
Joe Saraceno <honestcomrade@gmail.com>
2018-08-27 17:56:55 -0700

    deploy

--- services/users/manage.py
+++ services/users/manage.py
@@ -11,6 +11,13 @@ from project.api.models import User
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+@cli.command()
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email='michael@mherman.org'))
+    db.session.add(User(username='joe', email='joe@test.com'))
+    db.session.commit()
+
 
 @cli.command()
 def recreate_db():
